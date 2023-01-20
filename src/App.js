24401248cfa42d525069ac0bea6287f71bc40021
gjy0605 <gjy0605@gmail.com>
2023-01-20 10:52:02 +0900

    명언 리액트파일 (명언내용 들어가기 전)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+const proverbs = ['가나다', '라마바', '아사자', '차카타', '타파하']
+const getRandomIndex = (length) => {
+  return parseInt(Math.random() * length)
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {proverbs[getRandomIndex(proverbs.length)]}
       </header>
     </div>
   );
